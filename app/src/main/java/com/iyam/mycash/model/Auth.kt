3e183c1f0a704c61794236987c165f45f60cd1df
yudiatmoko <yudiatmokoilham@gmail.com>
2025-02-01 13:53:26 +0700

    Finishing Auth Feature

--- app/src/main/java/com/iyam/mycash/model/Auth.kt
+++ app/src/main/java/com/iyam/mycash/model/Auth.kt
@@ -2,7 +2,6 @@ package com.iyam.mycash.model
 
 import android.os.Parcelable
 import androidx.annotation.Keep
-import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
@@ -10,14 +9,9 @@ import kotlinx.serialization.Serializable
 @Keep
 @Serializable
 data class Auth(
-    @SerializedName("email")
     val email: String?,
-    @SerializedName("id")
     val id: String?,
-    @SerializedName("isVerified")
     val isVerified: Boolean?,
-    @SerializedName("name")
     val name: String?,
-    @SerializedName("token")
     val token: String?
 ) : Parcelable
--- app/src/main/java/com/iyam/mycash/model/User.kt
+++ app/src/main/java/com/iyam/mycash/model/User.kt
@@ -2,7 +2,6 @@ package com.iyam.mycash.model
 
 import android.os.Parcelable
 import androidx.annotation.Keep
-import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
@@ -10,20 +9,12 @@ import kotlinx.serialization.Serializable
 @Keep
 @Serializable
 data class User(
-    @SerializedName("id")
     val id: String?,
-    @SerializedName("name")
     val name: String?,
-    @SerializedName("email")
     val email: String?,
-    @SerializedName("phone_number")
     val phoneNumber: String?,
-    @SerializedName("image")
     val image: String?,
-    @SerializedName("is_verified")
     val isVerified: Boolean?,
-    @SerializedName("created_at")
     val createdAt: String?,
-    @SerializedName("updated_at")
     val updatedAt: String?
 ) : Parcelable
