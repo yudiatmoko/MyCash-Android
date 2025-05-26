a988c9342ce3baad9834ff09e394bbdbc687cfe3
yudiatmoko <yudiatmokoilham@gmail.com>
2025-05-26 16:40:03 +0700

    Remove padding bottom at bottom nav

--- app/src/main/java/com/iyam/mycash/ui/main/MainActivity.kt
+++ app/src/main/java/com/iyam/mycash/ui/main/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
         setBottomNavigation()
